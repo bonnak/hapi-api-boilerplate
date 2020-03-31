b4c3a243559b6d3c606a67b8bfffd53785cc533a
Willie Potgieter <willie658@gmail.com>
2020-03-31 12:59:32 +0200

    Initial commit of Hapi.js server. Basic server created (server.js) and nodemon set up in package.json

---
+++ src/server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+	const server = Hapi.server({
+		port: process.env.PORT || 3000,
+		host: '0.0.0.0',
+		routes: {
+			cors: true
+		}
+	});
+
+	await server.start();
+	console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', err => {
+	console.log(err);
+	process.exit(1);
+});
+
+init();
